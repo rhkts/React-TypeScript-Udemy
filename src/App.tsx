44800b45201b6de09acd7f39f035d3d61a2003f1
rhkts <tahagoo168@gmailc.com>
2023-07-31 22:17:23 +0900

    JSONPlaceholderからtodoのデータ取得(型指定はまだ)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import axios from "../node_modules/axios/index";
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
@@ -6,8 +6,15 @@ import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
 
 function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <>
+      <button onClick={onClickFetchData}>データ取得</button>
+      <hr />
       <Practice1 />
       <Practice2 />
       <Practice3 />
