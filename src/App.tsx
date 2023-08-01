561b69e85e8cb0dac38395bd11d503b43ed278ad
rhkts <tahagoo168@gmailc.com>
2023-08-01 19:54:35 +0900

    取得データの型定義を指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import axios from "../node_modules/axios/index";
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <>
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo, index) => (
+        <Todo title={todo.title} userId={todo.userId} />
+      ))}
       <hr />
       <Practice1 />
       <Practice2 />
