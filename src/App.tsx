275227749879173ea8e065246c7317d5256fdb44
rhkts <tahagoo168@gmailc.com>
2023-08-02 20:13:39 +0900

    カスタムフックを使わずにユーザー一覧を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
 import { useState } from "react";
 import axios from "../node_modules/axios/index";
 import "./App.css";
+import { UserCard } from "./components/UserCard";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
 import { Text } from "./Text";
 import { Todo } from "./Todo";
+import { UserApi } from "./types/api/userApi";
 import { TodoType } from "./types/todo";
 import { User } from "./types/user";
-import { UserProfile } from "./UserProfile";
+import { UserProfile } from "./types/userProfile";
 
 const user: User = {
   name: "namae",
   hobbies: ["映画", "散歩"],
 };
+
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<UserApi>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.city}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   const onClickFetchData = () => {
     axios
@@ -27,8 +56,21 @@ function App() {
   };
   return (
     <>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データ取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading....</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+      <hr />
       <Text color="red" fontSize="18px" />
-      <UserProfile user={user} />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
