b94d41d6f22eb7a4cbe8453c03bd8e276769825f
rhkts <tahagoo168@gmailc.com>
2023-08-02 20:19:04 +0900

    ユーザー一覧を取得するカスタムフックを作成

---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import axios from "../../node_modules/axios/index";
+import { UserApi } from "../types/api/userApi";
+import { UserProfile } from "../types/userProfile";
+
+//全ユーザー一覧を取得するカスタムフック
+export const useAllUsers = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<UserApi>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.city}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return {
+    getUsers,
+    userProfiles,
+    loading,
+    error,
+  };
+};
